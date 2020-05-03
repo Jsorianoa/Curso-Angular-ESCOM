8e9302a28a87580d9714545e21ba727602a4556e
Jorge Soriano <jorge.soriano@bwr.mx>
2020-05-03 02:32:55 -0500

    primer guardado del curso

---
+++ typescript/app.ts
@@ -0,0 +1,24 @@
+(function(){
+
+    /*let mensaje: string = 'Hola'; //const es para definir variables globales//let es para cambiar las variables
+    let numero: number = 123;    
+    let booleano: boolean = true;
+    let hoy: Date = new Date();
+
+    let cualquiercosa;
+    cualquiercosa = mensaje;
+    cualquiercosa = numero;
+    cualquiercosa = hoy;
+    cualquiercosa = booleano;
+    //if (true){
+     //   let mensaje = 'mundo';
+   // }*/
+    const nombre = 'Jorge';
+    const apellido = 'Soriano';
+    const edad = 26;
+
+    //const salida = nombre + " " + apellido + " ( " + edad + " ) ";
+   const salida = `${nombre} ${apellido} (${edad+10})`;
+    console.log(salida);
+     
+})();
